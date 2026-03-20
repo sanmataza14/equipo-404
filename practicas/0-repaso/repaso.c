e41eb72f7a19867ec78797d52a3e0b446c8771eb
SimonClaudel <simonclaudel2006@gmail.com>
2026-03-20 12:48:08 -0300

    Punto 3 y 4

--- practicas/0-repaso/repaso.c
+++ practicas/0-repaso/repaso.c
@@ -22,25 +22,73 @@ enum Referencia dondeEstaElPunto(int xc, int yc, int rc, int xp, int yp) {
     else{
         return EXTERNO;
     }
-
 }
 
 bool digitoEnNumero(long n, short d) {
 }
 
 enum Referencia dondeEstaElPuntoBis(struct Punto centroC, int rc, struct Punto p) {
+    int dx, dy, distanciaAlcuadrado, radioAlcuadrado;
+    dx = p.x - centroC.x;
+    dy = p.y - centroC.y;
+    distanciaAlcuadrado = (dx * dx) + (dy * dy);
+    radioAlcuadrado = (rc * rc);
+    if(radioAlcuadrado > distanciaAlcuadrado){
+        return INTERNO;
+    }
+    else if(radioAlcuadrado == distanciaAlcuadrado){
+        return EN_CIRCUNFERENCIA;
+    }
+    else{
+        return EXTERNO;
+    }
 }
 
 // Función para ordenar jugadores por cantidad de partidos jugados
 struct Jugador *jugadoresOrdenadosPorCantDePartidos(struct Jugador equipo[CANT_JUGADORES]) {
+    for (int i = 0; i < CANT_JUGADORES - 1; i++) {
+        for (int j = 0; j < CANT_JUGADORES - 1 - i; j++) {
+        if (equipo[j].partidosJugados < equipo[j+1].partidosJugados) {
+                struct Jugador aux = equipo[j];
+                equipo[j] = equipo[j+1];
+                equipo[j+1] = aux;
+            }
+        }
+    }
 }
 
 // Función para ordenar jugadores por edad
 struct Jugador *jugadoresOrdenadosPorEdad(struct Jugador equipo[CANT_JUGADORES]) {
+    for(int i = 0; i < CANT_JUGADORES - 1; i++){
+        for(int j = 0; j < CANT_JUGADORES - 1 - i; j++){
+            if(equipo[j].edad > equipo[j+1].edad){
+                struct Jugador aux = equipo[j];
+                equipo[j] = equipo[j+1];
+                equipo[j+1] = aux;
+            }
+        }
+    }
+    return equipo;
 }
 
 // Función para calcular el promedio de partidos jugados dada una edad
 float promedioDePartidosJugados(struct Jugador equipo[CANT_JUGADORES], int edad) {
+    int suma_partidos = 0;
+    int cant_jugadores = 0;
+    int promedio;
+    for(int i = 0; i < CANT_JUGADORES; i++){
+        if(equipo[i].edad == edad){
+            suma_partidos += equipo[i].partidosJugados;
+            cant_jugadores += 1;
+        }
+    }
+    if(cant_jugadores > 0){
+    promedio = suma_partidos / cant_jugadores;
+    return promedio;
+    }
+    else{
+        return 0;
+    }
 }
 
 Vivienda viviendaConMasHabitantes(int edificio[CANT_PISOS][CANT_DEPARTAMENTOS]) {
--- practicas/0-repaso/tests/test_03_donde_esta_el_punto_bis.c
+++ practicas/0-repaso/tests/test_03_donde_esta_el_punto_bis.c
@@ -8,6 +8,14 @@
 
 int main() {
     imprimir_titulo("Tests de Ejercicio 3");
-
+    struct Punto centro = {0, 0};
+    struct Punto p1 = {1, 1};
+    struct Punto p2 = {1, 0};
+    // Caso 1: debería ser INTERNO
+    assert(dondeEstaElPuntoBis(centro, 2, p1) == INTERNO);
+    // Caso 2: deberia ser EXTERNO
+    assert(dondeEstaElPuntoBis(centro, 1, p1) == EXTERNO);
+    // Caso 3: deberia ser EN_CIRCUNFERENCIA
+    assert(dondeEstaElPuntoBis(centro, 1, p2) == EN_CIRCUNFERENCIA);
     return 0;
 }
