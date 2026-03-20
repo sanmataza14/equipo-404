b492c61221ff401875be21bf081cdd3404f5fc09
SimonClaudel <simonclaudel2006@gmail.com>
2026-03-20 15:02:54 -0300

    Punto 1

--- practicas/0-repaso/tests/test_01_donde_esta_el_punto.c
+++ practicas/0-repaso/tests/test_01_donde_esta_el_punto.c
@@ -9,10 +9,9 @@
 int main() {
     imprimir_titulo("Tests de Ejercicio 1");
     //test 1: punto dentro de circunferencia
-    dondeEstaElPunto(0, 0, 2, 1, 1 ), INTERNO;
+    assert(dondeEstaElPunto(0, 0, 2, 1, 1 ) == INTERNO);
     //test 2; punto fuera circurferencia
-    dondeEstaElPunto(0, 0, 1, 1, 1 ), EXTERNO;
+    assert(dondeEstaElPunto(0, 0, 1, 1, 1 ) == EXTERNO);
     //test 3: punto sobre circunferencia
-    dondeEstaElPunto(0, 0, 1, 1, 0 ), EN_CIRCUNFERENCIA;
-    return 0;
+    assert(dondeEstaElPunto(0, 0, 1, 1, 0 ) == EN_CIRCUNFERENCIA);
 }
