ba279d5ad3027d600ea7ba323a4d51707d4d407a
SimonClaudel <simonclaudel2006@gmail.com>
2026-04-24 15:39:35 -0300

    Correcion del TP

--- practicas/1-recursividad/Trabajo practico integrador/mapa_mina.c
+++ practicas/1-recursividad/Trabajo practico integrador/mapa_mina.c
@@ -73,41 +73,38 @@ void buscar_ruta(char** celdas, int filas, int columnas, Posicion pos_actual, in
         return; 
     }
 
-    if (celdas[pos_actual.fila - 1][pos_actual.columna - 1] == 'S') {
-    if (!Mejor_solucion->encontrada || puntaje > Mejor_solucion->puntaje) {
+    camino_actual[pasos] = pos_actual;
+    char celda_actual = celdas[pos_actual.fila - 1][pos_actual.columna - 1];
+    if (celda_actual >= '0' && celda_actual <= '9') {
+            puntaje += (celda_actual - '0');
+            }
 
+    if (celda_actual == 'S') {
+        bool mejor_encontrado = false;
+        if (!Mejor_solucion->encontrada || puntaje > Mejor_solucion->puntaje || puntaje == Mejor_solucion->puntaje && pasos < Mejor_solucion->pasos) {
+                mejor_encontrado = true;
+            }
 
-        Mejor_solucion->puntaje = puntaje;
-        Mejor_solucion->pasos = pasos;
-
-
-        if (Mejor_solucion->encontrada == true){
-        free(Mejor_solucion->movimientos);
-        free(Mejor_solucion->camino);
-
-        Mejor_solucion->movimientos = NULL;
-        Mejor_solucion->camino = NULL;
-        }
+         if (mejor_encontrado) {
+            if (Mejor_solucion->encontrada) {
+                free(Mejor_solucion->movimientos);
+                free(Mejor_solucion->camino);
+            }
 
         Mejor_solucion->encontrada = true;
+        Mejor_solucion->puntaje = puntaje;
+        Mejor_solucion->pasos = pasos;
+        Mejor_solucion->largo_camino = pasos + 1;
 
         Mejor_solucion->movimientos = malloc((pasos + 1) * sizeof(char));
         strcpy(Mejor_solucion->movimientos, movimientos_actuales);
 
         Mejor_solucion->camino = malloc((pasos + 1) * sizeof(Posicion));
         memcpy(Mejor_solucion->camino, camino_actual, (pasos + 1) * sizeof(Posicion));
-
+        }
         return;
         }
-    }
-
-     char celda_actual = celdas[pos_actual.fila - 1][pos_actual.columna - 1];
-        if (celda_actual >= '0' && celda_actual <= '9') {
-            puntaje += (celda_actual - '0');
-            }
-
-        camino_actual[pasos] = pos_actual;
-
+    
         char valor_original = celdas[pos_actual.fila - 1][pos_actual.columna - 1];
         celdas[pos_actual.fila - 1][pos_actual.columna - 1] = 'X';
         Posicion nueva_pos;
@@ -143,34 +140,35 @@ void buscar_ruta(char** celdas, int filas, int columnas, Posicion pos_actual, in
     return;
     }
 
-    int main(){
-        Mapa* mapa = cargar_mapa("mapa.txt");
-        if (mapa == NULL) return 1;
+int main(){
+    Mapa* mapa = cargar_mapa("mapa.txt");
+    if (mapa == NULL) return 1;
 
-        int capacidad = mapa->filas * mapa->columnas;
+    int capacidad = mapa->filas * mapa->columnas;
 
-        Solucion mejor = { .encontrada = false, .puntaje = -1 };
-        mejor.movimientos = NULL;
-        mejor.camino = NULL;
+    Solucion mejor = { .encontrada = false, .puntaje = -1 };
+    mejor.movimientos = NULL;
+    mejor.camino = NULL;
 
-        char* mov_actuales = malloc((capacidad + 1) * sizeof(char));
-        Posicion* camino_actual = malloc(capacidad * sizeof(Posicion));
+    char* mov_actuales = malloc((capacidad + 1) * sizeof(char));
+    Posicion* camino_actual = malloc(capacidad * sizeof(Posicion));
+    mov_actuales[0] = '\0';
+    
+    buscar_ruta(mapa->celdas, mapa->filas, mapa->columnas, mapa->inicio, 0, 0, &mejor, mov_actuales, camino_actual);
 
-        buscar_ruta(mapa->celdas, mapa->filas, mapa->columnas, mapa->inicio, 0, 0, &mejor, mov_actuales, camino_actual);
+    free(mov_actuales);
+    free(camino_actual);
 
-        free(mov_actuales);
-        free(camino_actual);
-
-        for(int i=0; i < mapa->filas; i++){
-            free(mapa->celdas[i]);
-        }
-        free(mapa->celdas);
-        free(mapa);
-
-        if(mejor.encontrada){
-            free(mejor.movimientos);
-            free(mejor.camino);
-        }
-
-        return 0;   
+    for(int i=0; i < mapa->filas; i++){
+        free(mapa->celdas[i]);
     }
+    free(mapa->celdas);
+    free(mapa);
+
+    if(mejor.encontrada){
+        free(mejor.movimientos);
+        free(mejor.camino);
+    }
+
+    return 0;   
+}
