647e50683af0653c1d24509be96b3011fe288188
SimonClaudel <simonclaudel2006@gmail.com>
2026-05-07 02:28:39 -0300

    Correcciones punto 9 y test

--- practicas/1-recursividad/tests/test_divisible_por_7.c
+++ practicas/1-recursividad/tests/test_divisible_por_7.c
@@ -10,5 +10,7 @@ int main() {
     imprimir_titulo("Tests para divisiblePor7");
     assert(esDivisiblePor7(32291) == true);
     assert(esDivisiblePor7(110) == false);
+    assert(esDivisiblePor7(-700) == true);
+    assert(esDivisiblePor7(20) == false);
     return 0;
 }
