9ec037a3d4b012fe323d47dae29e03479b359d77
SimonClaudel <simonclaudel2006@gmail.com>
2026-05-22 01:44:30 -0300

    Punto de promedio

--- practicas/3-listas/practica_listas.c
+++ practicas/3-listas/practica_listas.c
@@ -5,6 +5,29 @@
 #include "tipo_elemento/tipo_elemento.h"
 #include "listas/listas.h"
 
+void promedioRecAux(Lista l, Iterador p, double *suma, int *contador){
+    if(p == fin(l)){
+        return;
+    }
+    TipoElemento te = recuperar(l, p);
+    *suma += te_recuperar(te);
+    *contador += 1;
+    promedioRecAux(l, siguiente(p), suma, contador);
+}
+
+double promedio(Lista l){
+    if(primero(l) == fin(l)){
+        return 0.0;
+    }
+double suma = 0;
+int contador = 0;
+Iterador p = primero(l);
+
+promedioRecAux(l, p, &suma, &contador);
+
+return suma / contador;
+}
+
 Lista invertirListaRec(Iterador iter) {
 }
 
